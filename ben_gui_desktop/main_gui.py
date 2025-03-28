45b806b8380ff26f24a659dd24c3cbf1dae23c91
kermach11 <paralelnomax@gmail.com>
2025-03-28 21:45:06 +0100

    ✅ Auto-commit: update_code

--- ben_gui_desktop/main_gui.py
+++ ben_gui_desktop/main_gui.py
@@ -6,6 +6,7 @@ import os
 from widgets.git_log_viewer import GitLogViewer
 from template_manager import render_template
 from widgets.user_profile_panel import UserProfilePanel
+from widgets.template_editor import TemplateEditor
 from widgets.history_viewer import HistoryViewer
 import json
 
